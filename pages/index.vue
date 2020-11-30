475bc0db1f3c3d56f663e0ffc9da133ecac3116a
frederikagger <frederikagger@hotmail.com>
2020-11-30 15:23:11 +0100

    inital commit for frontend

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header />
+    <div class="container mx-auto">
+      <h1 class="text-white text-6xl font-semibold text-center mt-5">Welcome</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "../components/Header.vue";
+export default {
+  components: { Header },
+  middleware: "auth"
+};
+</script>
+
+<style></style>
