369725a0b059dc66fe69de184d8c77992d485bab
frederikagger <frederikagger@hotmail.com>
2020-12-01 12:57:04 +0100

    new pages created

---
+++ pages/profile/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Header />
+    <div class="container mx-auto">
+        <h1 class="text-center text-white font-semibold tracking-wide text-2xl md:text-4xl mt-4">Profile</h1>
+        <h3 class="">Name: {{user.name}}</h3>
+
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "../../components/Header.vue";
+export default {
+  components: { Header },
+    middleware: "auth",
+};
+</script>
+
+<style></style>
---
+++ pages/settings/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Header />
+    <div class="container mx-auto">
+        <h1 class="text-center text-white font-semibold tracking-wide text-2xl md:text-4xl mt-4">Settings</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "../../components/Header.vue";
+export default {
+  components: { Header },
+    middleware: "auth",
+};
+</script>
+
+<style></style>
