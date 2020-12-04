9fffe1018027202c16276644d786963c386f41ca
frederikagger <frederikagger@hotmail.com>
2020-12-04 17:00:31 +0100

    refactoring: header pulled in to layout to clean up

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,19 @@
 <template>
-  <div class="bg-gradient-to-l from-indigo-500 to-pink-500 h-screen">
+  <div class="bg-gradient-to-l from-indigo-500 to-pink-500 min-h-screen h-full">
+    <myHeader v-if="token"/>
     <Nuxt />
   </div>
 </template>
 
-<style>
-
-</style>
+<script>
+import myHeader from '../components/Header.vue'
+export default {
+  components: { myHeader },
+  computed: {
+    token() {
+      return this.$store.getters.token;
+    }
+  }
+  
+}
+</script>
