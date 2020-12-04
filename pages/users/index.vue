16bbf9c1688b7ce74d15d8962fbccaf6df0fe646
frederikagger <frederikagger@hotmail.com>
2020-12-04 13:07:38 +0100

    feat: picture now shown and own profile is filtered

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -6,10 +6,14 @@
       <h1 class="text-white mt-10 text-5xl text-center">Users</h1>
 
       <div v-for="user in users" :key="user._id">
-        <div class="max-w-sm mx-auto bg-indigo-700 p-5 my-3 rounded-lg">
+        <div v-show="loggedInUser._id !== user._id" class="max-w-sm mx-auto bg-indigo-700 p-5 my-3 rounded-lg">
           <div class="text-white text-xl">
             {{ user.firstname + " " + user.lastname }}
           </div>
+          <img
+                class="h-8 w-8 rounded-full"
+                :src="[user.profilePicURL ? user.profilePicURL : require('~/assets/anonymous.svg')]"
+                alt="profile picture">
         </div>
       </div>
     </div>
@@ -21,6 +25,11 @@ import myHeader from "../../components/Header.vue";
 export default {
   components: { myHeader },
   middleware: "auth",
+  computed: {
+     loggedInUser() {
+      return this.$store.getters.user;
+    }
+  },
   data() {
     return {
       users: []
@@ -29,6 +38,7 @@ export default {
   async created() {
     try {
       this.users = await this.$axios.$get("/users");
+      console.log(this.users);
     } catch (error) {
       console.log(error);
     }
