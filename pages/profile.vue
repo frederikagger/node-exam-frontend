ca8f4675ffe92f69f57eeebb451c123bc045921b
frederikagger <frederikagger@hotmail.com>
2020-12-08 17:29:28 +0100

    feat: profile page created

---
+++ pages/profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="flex flex-col absolute inset-y-10 left-0">
+      <nuxt-link :to="'/profile/picture'" class="text-indigo-700 font-semibold border-b-2 border-gray-800 tracking-wide text-2xl m-2 p-2 hover:text-white">Picture</nuxt-link>
+      <nuxt-link :to="'/profile/data'" class="text-indigo-700 font-semibold border-b-2 border-gray-800 tracking-wide text-2xl m-2 p-2 hover:text-white">Profile data</nuxt-link>
+    </div>
+    <div>
+          <nuxt-child/>
+    </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+      middleware: "auth",
+};
+</script>
+
+<style></style>
