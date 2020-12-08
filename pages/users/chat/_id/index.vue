dab91ab507658a9db7d0aa2ab290b346aae452cd
frederikagger <frederikagger@hotmail.com>
2020-12-08 17:27:49 +0100

    refactoring: usercard component created

---
+++ pages/users/chat/_id/index.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container mx-auto">
+    <div
+      class="flex flex-col mx-auto max-w-xs md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl"
+    >
+      <h1
+        class="text-5xl mx-auto font-mono text-white mt-5 sm:mt-10 xl:mb-10 mb-5 font-semibold"
+      >
+        Private chat
+      </h1>
+      <textarea
+        v-model="chat"
+        class="bg-white rounded-lg mb-10 mx-auto w-full h-64 sm:h-96 p-2 overflow-auto"
+      ></textarea>
+
+      <input
+        class="py-1 px-4 focus:placeholder-gray-600 mx-auto w-full border mb-2 placeholder-gray-400 text-lg text-black  bg-white rounded-md border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+        v-model="message"
+        type="text"
+        autocomplete="off"
+        placeholder="Message.."
+      />
+      <button
+        type="button"
+        class="py-2 px-4 bg-blue-500 text-white text-lg mx-auto w-full font-semibold rounded-lg shadow-lg  hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-700 focus:ring-opacity-75"
+        @click="send"
+      >
+        Send
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  middleware: "auth",
+  data() {
+    return {
+      chat: "",
+      message: ""
+    };
+  },
+  mounted() {
+    this.socket = this.$nuxtSocket({});
+    this.socket.on("server emit", (msg, cb) => {
+      const { message } = msg;
+      this.chat = this.chat + message;
+    });
+  },
+  methods: {
+    send() {
+      if (this.message) {
+        this.socket.emit(
+          "chat message",
+          {
+            message: this.message
+          },
+          resp => {
+            console.log(resp);
+            /* Handle response, if any */
+          }
+        );
+        this.message = "";
+      }
+    }
+  }
+};
+</script>
--- pages/users/index.vue
+++ pages/users/index.vue
@@ -1,52 +1,29 @@
 <template>
-    <div class="container mx-auto">
-      <h1 class="text-white mt-4 text-5xl text-center">Users</h1>
-
-      <div v-for="user in users" :key="user._id">
-        <div
-          v-show="loggedInUser._id !== user._id"
-          class="max-w-sm mx-auto flex justify-between bg-indigo-700 p-5 my-3 rounded-lg cursor-pointer"
-        >
-          <div class="flex flex-col">
-            <div class="text-white text-xl">
-              {{ user.firstname + " " + user.lastname }}
-            </div>
-            <div class="text-gray-200">
-              {{ user.country }}
-            </div>
-          </div>
-
-          <img
-            class="h-14 w-14 rounded-full"
-            :src="[
-              user.profilePicURL
-                ? user.profilePicURL
-                : require('~/assets/anonymous.svg')
-            ]"
-            alt="profile picture"
-          />
-        </div>
-      </div>
-    </div>
+  <div class="container mx-auto">
+    <h1 class="text-white mt-4 text-5xl text-center">Users</h1>
+    <user-card v-for="user in users" :user="user" :key="user._id" />
+    <pagination />
+  </div>
 </template>
 
 <script>
+import pagination from "../../components/pagination.vue";
+import UserCard from "../../components/userCard.vue";
 export default {
+  components: { pagination, UserCard },
   middleware: "auth",
-  computed: {
-    loggedInUser() {
-      return this.$store.getters.user;
-    }
-  },
   data() {
     return {
-      users: []
+      users: [],
+      limit: 5,
+      page: 1
     };
   },
   async created() {
     try {
-      this.users = await this.$axios.$get("/users");
-      console.log(this.users);
+      this.users = await this.$axios.$get(
+        "/users?limit=" + this.limit + "&page=" + this.page
+      );
     } catch (error) {
       console.log(error);
     }
