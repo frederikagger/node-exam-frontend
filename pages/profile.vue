ebe927e3f662651653e5d4767eb4446db91fa2f8
frederikagger <frederikagger@hotmail.com>
2020-12-08 22:22:25 +0100

    reponsive classes added

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="flex flex-col absolute inset-y-10 left-0">
+    <div class="flex flex-col md:absolute relative inset-y-10 left-0">
       <nuxt-link :to="'/profile/picture'" class="text-indigo-700 font-semibold border-b-2 border-gray-800 tracking-wide text-2xl m-2 p-2 hover:text-white">Picture</nuxt-link>
       <nuxt-link :to="'/profile/data'" class="text-indigo-700 font-semibold border-b-2 border-gray-800 tracking-wide text-2xl m-2 p-2 hover:text-white">Profile data</nuxt-link>
     </div>
